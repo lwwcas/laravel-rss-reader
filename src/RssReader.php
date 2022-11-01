54303606634e8c6f28b9217ee9bd5c12fd383dcd
Lucas Duarte <lwwcas@gmail.com>
2022-11-01 17:06:55 +0000

    refactor: optimize RssReader file for testing

--- src/RssReader.php
+++ src/RssReader.php
@@ -3,13 +3,14 @@
 namespace Lwwcas\LaravelRssReader;
 
 use Illuminate\Support\Facades\Config;
+use Lwwcas\LaravelRssReader\Feeds\BaseFeed;
 use SimpleXMLElement;
 
 class RssReader
 {
-    private const NAMESPACE = 'Lwwcas\LaravelRssReader';
+    private const NAMESPACE = 'Lwwcas\LaravelRssReader\\';
 
-    public function feed(string $rssFeed)
+    public function feed(string $rssFeed): array
     {
         $rssClass = $this->getActiveFeed($rssFeed);
 
@@ -17,36 +18,24 @@ class RssReader
             throw new \LogicException('Your RSS feed is not active in your settings file.');
         }
 
-        $url = $rssClass->sourceUrl();
+        $feed = $this->getNormalizeFeed($rssClass);
+        $root = $this->generateRootFeed($rssClass, $feed);
+        $root['articles'] = $this->generateArticlesFeed($rssClass, $feed);
+
+        $root = $rssClass->feedCreated($root);
+
+        return $root;
+    }
+
+    private function generateArticlesFeed(BaseFeed $rssClass, array $feed): array
+    {
         $setup = $rssClass->sourceSetup();
-        $metadata = $rssClass->sourceMetadata();
         $articleData = $rssClass->sourceArticleData();
         $articleSource = $rssClass->sourceArticle();
 
-        $feed = $this->readFromUrl($url);
-        $feed = $this->toArray($feed);
-        $feed = $feed[$setup['core']];
-
-        $feedDate = $rssClass->dateParse($feed[$metadata['lastUpdate']]);
-        $root = [
-            'url' => $feed[$setup['url']],
-            'title' => $feed[$setup['title']],
-            'description' => $feed[$setup['description']],
-            'metadata' => [
-                'generator' => $feed[$metadata['generator']],
-                'language' => $feed[$metadata['language']],
-                'lastUpdate' => $feedDate,
-            ],
-        ];
-
-        if (array_key_exists($setup['image'], $feed) === true) {
-            if (array_key_exists('url', $feed[$setup['image']])) {
-                $root['image'] = $feed[$setup['image']]['url'];
-            }
-        }
-
-        $articles = [];
         $articleDataFormat = $this->config('defaultArticlesDateFormat');
+        $articles = [];
+
         foreach ($feed[$setup['articles']] as $key => $article) {
             $articleDate = $rssClass->dateParse($article[$articleSource['date']], $articleDataFormat);
             $articles[$key] = [
@@ -65,11 +54,45 @@ class RssReader
             $articles[$key]['data'] = $articleDataList;
         }
 
-        $root['articles'] = $articles;
+        return $articles;
+    }
 
-        $root = $rssClass->feedCreated($root);
+    private function generateRootFeed(BaseFeed $rssClass, array $feed): array
+    {
+        $setup = $rssClass->sourceSetup();
+        $metadata = $rssClass->sourceMetadata();
+
+        $feedDate = $rssClass->dateParse($feed[$metadata['lastUpdate']]);
+
+        $root = [
+            'url' => $feed[$setup['url']],
+            'title' => $feed[$setup['title']],
+            'description' => $feed[$setup['description']],
+            'metadata' => [
+                'generator' => $feed[$metadata['generator']],
+                'language' => $feed[$metadata['language']],
+                'lastUpdate' => $feedDate,
+            ],
+        ];
+
+        if (array_key_exists($setup['image'], $feed) === true) {
+            if (array_key_exists('url', $feed[$setup['image']])) {
+                $root['image'] = $feed[$setup['image']]['url'];
+            }
+        }
 
         return $root;
+    }
+
+    private function getNormalizeFeed(BaseFeed $rssClass): array
+    {
+        $url = $rssClass->sourceUrl();
+        $setup = $rssClass->sourceSetup();
+
+        $feed = $this->readFromUrl($url);
+        $feed = $this->toArray($feed);
+
+        return $feed[$setup['core']];
     }
 
     private function getActiveFeed(string $rssFeed)
@@ -85,7 +108,7 @@ class RssReader
             return null;
         }
 
-        if ($arrayFeedSearch != false && $activeRss[$arrayFeedSearch] === $rssFeed) {
+        if ($arrayFeedSearch !== false && $activeRss[$arrayFeedSearch] === $rssFeed) {
             return $this->getRssClass($rssFeed);
         }
 
@@ -94,7 +117,7 @@ class RssReader
 
     private function getRssClass(string $rssFeed)
     {
-        $namespace = RssReader::NAMESPACE . '\\' . 'feeds\\';
+        $namespace = RssReader::NAMESPACE . 'feeds\\';
         $rssClassName = $namespace . str_replace('-', '', ucwords($rssFeed, '-'));
 
         return (new $rssClassName());
@@ -107,7 +130,11 @@ class RssReader
      */
     public function readFromUrl(string $url)
     {
-        $content = file_get_contents($url);
+        $content = @file_get_contents($url);
+
+        if ($content === false) {
+            throw new \LogicException('The RSS feed url is not valid.');
+        }
 
         // Instantiate XML element
         $xmlElement = new SimpleXMLElement($content);
