8e25de43ece9388c5874990284c4f0cf92598295
Lucas Duarte <lwwcas@gmail.com>
2022-11-01 00:49:59 +0000

    chore: wip RssReader file

--- src/RssReader.php
+++ src/RssReader.php
@@ -7,8 +7,6 @@ use SimpleXMLElement;
 
 class RssReader
 {
-    private const NAMESPACE = 'Lwwcas\LaravelRssReader';
-
     /**
      * RssReader constructor.
      */
@@ -17,15 +15,86 @@ class RssReader
         //
     }
 
+    public function feed(string $rssFeed)
+    {
+        $rssClass = $this->getActiveFeed($rssFeed);
+
+        if ($rssClass === null) {
+            throw new \LogicException('Your RSS feed is not active in your settings file.');
+        }
+
+        $url = $rssClass->sourceUrl();
+        $setup = $rssClass->sourceSetup();
+        $metadata = $rssClass->sourceMetadata();
+        $articleData = $rssClass->sourceArticleData();
+        $articleSource = $rssClass->sourceArticle();
+
+        $feed = $this->readFromUrl($url);
+        $feed = $this->toArray($feed);
+        $feed = $feed[$setup['core']];
+
+        $feedDate = $rssClass->dateParse($feed[$metadata['lastUpdate']]);
+        $root = [
+            'url' => $feed[$setup['url']],
+            'title' => $feed[$setup['title']],
+            'description' => $feed[$setup['description']],
+            'metadata' => [
+                'generator' => $feed[$metadata['generator']],
+                'language' => $feed[$metadata['language']],
+                'lastUpdate' => $feedDate,
+            ],
+        ];
+
+        if (array_key_exists($setup['image'], $feed) === true) {
+            if (array_key_exists('url', $feed[$setup['image']])) {
+                $root['image'] = $feed[$setup['image']]['url'];
+            }
+        }
+
+        $articles = [];
+        $articleDataFormat = $this->config('defaultArticlesDateFormat');
+        foreach ($feed[$setup['articles']] as $key => $article) {
+            $articleDate = $rssClass->dateParse($article[$articleSource['date']], $articleDataFormat);
+            $articles[$key] = [
+                'url' => $article[$articleSource['url']],
+                'title' => $article[$articleSource['title']],
+                'description' => $article[$articleSource['description']],
+                'date' => $articleDate,
+                'image' => null,
+            ];
+
+            $articleDataList = [];
+            foreach ($articleData as $data) {
+                $articleDataList[$data] = $article[$data];
+            }
+
+            $articles[$key]['data'] = $articleDataList;
+        }
+
+        $root['articles'] = $articles;
+
+        $root = $rssClass->feedCreated($root);
+
+        return $root;
+    }
+
+    private function getActiveFeed(string $rssFeed)
+    {
+        $activeRss = $this->config('active-rss');
+
+        if (array_key_exists($rssFeed, $activeRss) === false) {
+            return null;
+        }
+
+        return (new $activeRss[$rssFeed]());
+    }
+
     /**
      * Used to parse an RSS feed.
      *
-     * @param        $url
-     * @param string $configuration
-     * @param array  $options
+     * @param $url
      */
-    // public static function read($url, $configuration = 'default', array $options = [])
-    public function read(string $url)
+    public function readFromUrl(string $url)
     {
         $content = file_get_contents($url);
 
@@ -35,47 +104,22 @@ class RssReader
         return $xmlElement;
     }
 
-    public function feed(string $rssFeed)
+    public function toArray(SimpleXMLElement $xmlElement = null)
     {
-        if ($this->getActiveFeed($rssFeed) === null) {
-            throw new \LogicException('Your RSS feed is not active in your settings file.');
+        if (!$xmlElement->children()) {
+            return (string) $xmlElement;
         }
 
-        $rssClass = $this->getRssClass($rssFeed);
-        $url = $rssClass->url();
-        $setup = $rssClass->setup();
-
-        $feed = $this->read($url);
-        $articles = [];
-
-        // return $feed->{$setup['core']}->{$setup['articles']}->description;
-
-        // foreach ($feed->{$setup['core']}->{$setup['articles']} as  $key => $article) {
-        //     $articles[$key] = (array)$article;
-        // }
-
-        dd($feed);
-    }
-
-    private function getRssClass(string $rssFeed)
-    {
-        $namespace = RssReader::NAMESPACE . '\\' . 'feeds\\';
-        $rssClassName = $namespace . str_replace('-', '', ucwords($rssFeed, '-'));
-        $rssClass = null;
-
-        $rssClass = (new $rssClassName());
-
-        return $rssClass;
-    }
-
-    private function getActiveFeed(string $rssFeed)
-    {
-        $activeRss = $this->config('active-rss');
-        if (!in_array($rssFeed, $activeRss)) {
-            return null;
+        $xmlArray = [];
+        foreach ($xmlElement->children() as $tag => $child) {
+            if (count($xmlElement->$tag) === 1) {
+                $xmlArray[$tag] = $this->toArray($child);
+            } else {
+                $xmlArray[$tag][] = $this->toArray($child);
+            }
         }
 
-        return $activeRss;
+        return $xmlArray;
     }
 
     /**
