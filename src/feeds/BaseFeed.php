afa0c1a8ec59bddc8ecf8ee3cc597e577790ee14
Lucas Duarte <lwwcas@gmail.com>
2022-11-01 00:48:46 +0000

    chore: update functions of basefeed file, and add a prefix

--- src/feeds/BaseFeed.php
+++ src/feeds/BaseFeed.php
@@ -20,9 +20,20 @@ abstract class BaseFeed
 
     protected $language = null;
 
+    protected $metadata = [];
+
     protected $setup = [];
 
-    public function generator()
+    protected $article = [];
+
+    protected $articleData = [];
+
+    public function id()
+    {
+        return $this->id;
+    }
+
+    public function sourceGenerator()
     {
         $config = $this->configParameter('generator');
         if ($config != null) {
@@ -32,7 +43,7 @@ abstract class BaseFeed
         return $this->generator;
     }
 
-    public function url()
+    public function sourceUrl()
     {
         $config = $this->configParameter('url');
         if ($config != null) {
@@ -42,7 +53,7 @@ abstract class BaseFeed
         return $this->url;
     }
 
-    public function image()
+    public function sourceImage()
     {
         $config = $this->configParameter('image');
         if ($config != null) {
@@ -52,7 +63,7 @@ abstract class BaseFeed
         return $this->image;
     }
 
-    public function title()
+    public function sourceTitle()
     {
         $config = $this->configParameter('title');
         if ($config != null) {
@@ -62,7 +73,17 @@ abstract class BaseFeed
         return $this->title;
     }
 
-    public function language()
+    public function sourceDescription()
+    {
+        $config = $this->configParameter('description');
+        if ($config != null) {
+            return $config;
+        }
+
+        return $this->description;
+    }
+
+    public function sourceLanguage()
     {
         $config = $this->configParameter('language');
         if ($config != null) {
@@ -72,7 +93,17 @@ abstract class BaseFeed
         return $this->language;
     }
 
-    public function setup()
+    public function sourceMetadata()
+    {
+        $config = $this->configParameter('metadata');
+        if ($config != null) {
+            return $config;
+        }
+
+        return $this->metadata;
+    }
+
+    public function sourceSetup()
     {
         $config = $this->configParameter('setup');
         if ($config != null) {
@@ -82,15 +113,69 @@ abstract class BaseFeed
         return $this->setup;
     }
 
+    public function sourceArticle()
+    {
+        $config = $this->configParameter('article');
+        if ($config != null) {
+            return $config;
+        }
+
+        return $this->article;
+    }
+
+    public function sourceArticleData()
+    {
+        $config = $this->configParameter('articleData');
+        if ($config != null) {
+            return $config;
+        }
+
+        return $this->articleData;
+    }
+
+    public function sourceAll()
+    {
+        return [
+            'id' => $this->id(),
+            'generator' => $this->sourceGenerator(),
+            'url' => $this->sourceUrl(),
+            'image' => $this->sourceImage(),
+            'title' => $this->sourceTitle(),
+            'description' => $this->sourceDescription(),
+            'language' => $this->sourceLanguage(),
+            'metadata' => $this->sourceMetadata(),
+            'setup' => $this->sourceSetup(),
+            'article' => $this->sourceArticle(),
+            'articleData' => $this->sourceArticleData(),
+        ];
+    }
+
+    public function feedCreated(array $feed = [])
+    {
+        return $feed;
+    }
+
+    public function dateParse(string $date, ?string $format = null)
+    {
+        $dateFormat = $format === null ? $this->config('defaultDateFormat') : $format;
+        $date = strtotime($date);
+        return date($dateFormat, $date);
+    }
+
     private function configParameter($config)
     {
         $rssId = $this->id . '.';
-        $config = Config::get('laravel-rss-reader.config-rss.' . $rssId . $config);
+        $config = $this->config('config-rss.' . $rssId . $config);
 
         if ($config === null) {
             return null;
         }
 
         return $config;
+    }
+
+    private function config(string $config)
+    {
+        return Config::get('laravel-rss-reader.' . $config);
     }
 }
