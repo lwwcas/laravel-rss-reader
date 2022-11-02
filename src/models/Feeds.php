f01cc8c73f9ff55e02fe60857d6b2cfbd03c23f6
Lucas Duarte <lwwcas@gmail.com>
2022-11-02 11:08:56 +0000

    feat: create model to feeds

---
+++ src/models/Feeds.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Lwwcas\LaravelCountries\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
+
+class Feeds extends Model
+{
+    use HasFactory;
+
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'lw_feed_logs';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'uuid',
+        'name',
+        'key',
+        'action',
+        'read_at',
+    ];
+
+    /**
+     * The "booting" method of the model.
+     *
+     * @return void
+     */
+    public static function boot()
+    {
+        parent::boot();
+        self::creating(function ($model) {
+            $model->uuid = (string) Str::uuid();
+        });
+    }
+}
