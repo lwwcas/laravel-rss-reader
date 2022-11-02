5c9404c729cfe3582593b3ff6e7e65ed299d8673
Lucas Duarte <lwwcas@gmail.com>
2022-11-02 11:08:26 +0000

    feat: create migrations tables

---
+++ src/database/migrations/2022_11_01_194648_create_lw_feeds_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class () extends Migration {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('lw_feeds', function (Blueprint $table) {
+            $table->id();
+            $table->uuid('uuid');
+            $table->string('name')->nullable()->comment('The name of RSS feed provider');
+            $table->string('key')->unique()->index()->comment('The key | id on config file');
+            $table->timestamp('read_at')->comment('Last time the original RSS was accessed');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('lw_feeds');
+    }
+};
---
+++ src/database/migrations/2022_11_01_194702_create_lw_feed_articles_table.php
@@ -0,0 +1,50 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Query\Expression;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class () extends Migration {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('lw_feed_articles', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('feed_id')->unsigned();
+            $table->uuid('uuid');
+            $table->string('url');
+            $table->string('title');
+            $table->text('description');
+            $table->string('image');
+            $table->string('language');
+            $table->json('data')
+                        ->default(new Expression('(JSON_ARRAY())'))
+                        ->comment('The extra information of the saved article');
+            $table->boolean('active')
+                        ->default(true)
+                        ->comment('Defines if the article is able to be visible');
+            $table->boolean('black_list')
+                        ->default(false)
+                        ->comment('The article is blacklisted if there are offensive words');
+            $table->timestamp('date')->nullable();
+            $table->timestamps();
+
+            $table->foreign('feed_id')->references('id')->on('lw_feeds')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('lw_feed_articles');
+    }
+};
