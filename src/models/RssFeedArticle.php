aba0b3fbe16e94c86b08369a9f0690a461af75f3
Lucas Duarte <lwwcas@gmail.com>
2022-11-06 06:19:01 +0000

    feat: create read and yesterday methods on rss feed file

--- src/models/RssFeedArticle.php
+++ src/models/RssFeedArticle.php
@@ -2,14 +2,32 @@
 
 namespace Lwwcas\LaravelRssReader\Models;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Str;
 use Lwwcas\LaravelRssReader\Casts\Json;
+use Lwwcas\LaravelRssReader\Concerns\ConfigFeed;
 
 class RssFeedArticle extends Model
 {
     use HasFactory;
+    use ConfigFeed;
+
+    public $readingLimit = 20;
+
+    public $defaultArticlesDateFormat = null;
+
+    public $feedKey = null;
+
+    public $onBlackList = true;
+
+    public $activeArticle = true;
+
+    public $feedQuery = null;
+
+    public $rssFeedIdCached = true;
 
     /**
      * The table associated with the model.
@@ -60,6 +78,16 @@ class RssFeedArticle extends Model
         'black_list' => 'boolean',
     ];
 
+    public function __construct()
+    {
+        $_readingLimit = $this->config('article-reading-limit');
+        $_defaultArticlesDateFormat = $this->config('default-articles-date-format');
+
+        $this->readingLimit = $_readingLimit;
+        $this->defaultArticlesDateFormat = $_defaultArticlesDateFormat;
+        $this->feedQuery = $this;
+    }
+
     /**
      * The "booting" method of the model.
      *
@@ -80,5 +108,60 @@ class RssFeedArticle extends Model
     public function feed()
     {
         return $this->belongsTo(RssFeed::class, 'feed_id');
+    }
+
+    public function read(string $feedKey)
+    {
+        $rssFeedId = $this->getRssFeedId($feedKey);
+
+        $this->feedQuery = $this->with('feed')
+            ->where('feed_id', $rssFeedId);
+
+        return $this;
+    }
+
+    public function yesterday()
+    {
+        $yesterday = Carbon::yesterday()
+            ->format($this->defaultArticlesDateFormat);
+        $endDay = Carbon::yesterday()
+            ->addHours(23)
+            ->addMinutes(59)
+            ->addSeconds(59)
+            ->format($this->defaultArticlesDateFormat);
+
+        $this->feedQuery = $this->whereBetween('date', [$yesterday, $endDay]);
+        return $this;
+    }
+
+    protected function getRssFeedId(string $feedKey)
+    {
+        $this->feedKey = $feedKey;
+        $cacheKey = 'lw-rss-feed-' . $feedKey . '-id';
+
+        if (Cache::has($cacheKey) === true && $this->rssFeedIdCached === true) {
+            return Cache::get($cacheKey);
+        }
+
+        $rssFeed = RssFeed::select('id', 'key')
+                ->where('key', $this->feedKey)
+                ->first();
+
+        if ($rssFeed === null) {
+            throw new \LogicException('The RSS feed key is not valid.');
+        }
+
+        $rssFeedId = $rssFeed->id;
+
+        if (Cache::has($cacheKey) === false && $this->rssFeedIdCached === true) {
+            Cache::put($cacheKey, $rssFeedId, Carbon::now()->addDays(15));
+            return $rssFeedId;
+        }
+
+        if (Cache::has($cacheKey) === true) {
+            Cache::forget($cacheKey);
+        }
+
+        return $rssFeedId;
     }
 }
