ce350bd70e95f192ad18afb6cbd311f81391bef1
Lucas Duarte <lwwcas@gmail.com>
2022-11-14 10:02:28 +0000

    chore: remove methods duplicated from feed article

--- src/models/RssFeedArticle.php
+++ src/models/RssFeedArticle.php
@@ -126,63 +126,6 @@ class RssFeedArticle extends Model
         return $this;
     }
 
-    public function addCustomFilter(array $customFilter)
-    {
-        return $this->update(['custom' => $customFilter]);
-    }
-
-    public function addNewCustomFilter(array $customFilter)
-    {
-        $oldCustomFilter = $this->select('custom')->limit(1)->first();
-        if ($oldCustomFilter === null) {
-            return $this->addCustomFilter($customFilter);
-        }
-
-        $oldCustomFilter = $oldCustomFilter->custom;
-        $newBadWordsList = array_merge($oldCustomFilter, $customFilter);
-        return $this->addCustomFilter($newBadWordsList);
-    }
-
-    public function clearCustomFilter()
-    {
-        $this->update(['custom' => []]);
-    }
-
-    public function addBadWords(array $badWords)
-    {
-        return $this->update(['bad_words' => $badWords]);
-    }
-
-    public function addNewBadWords(array $badWords)
-    {
-        $oldBadWords = $this->select('bad_words')->limit(1)->first();
-        if ($oldBadWords === null) {
-            return $this->addBadWords($badWords);
-        }
-
-        $oldBadWords = $oldBadWords->bad_words;
-        $newBadWordsList = array_merge($oldBadWords, $badWords);
-        return $this->addBadWords($newBadWordsList);
-    }
-
-    public function clearBadWords()
-    {
-        $this->update(['bad_words' => []]);
-    }
-
-    public function addOnBlacklist()
-    {
-        return $this->update(['black_list' => true]);
-    }
-
-    public function removeOfBlacklist()
-    {
-        return \Illuminate\Support\Facades\DB::transaction(function () {
-            $this->update(['black_list' => false]);
-            $this->update(['bad_words' => []]);
-        });
-    }
-
     public function active()
     {
         return $this->update(['active' => true]);
