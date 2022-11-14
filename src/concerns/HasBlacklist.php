b4144ca0b26ffb7612e862ccde62c9ad0e6ca69f
Lucas Duarte <lwwcas@gmail.com>
2022-11-14 11:03:28 +0000

    fix: fix blacklist tait file

--- src/concerns/HasBlacklist.php
+++ src/concerns/HasBlacklist.php
@@ -6,12 +6,12 @@ trait HasBlacklist
 {
     public function isOnBlacklist(): bool
     {
-        $query = $this->select('black_list')->first();
-        if ($query === null) {
+        $blacklist = $this->black_list;
+        if ($blacklist === null) {
             return false;
         }
 
-        return (bool) $query->black_list;
+        return (bool) $blacklist;
     }
 
     public function addOnBlacklist()
@@ -29,12 +29,12 @@ trait HasBlacklist
 
     public function badWords()
     {
-        $query = $this->select('bad_words')->first();
-        if ($query === null) {
+        $badWords = $this->bad_words;
+        if ($badWords === null) {
             return [];
         }
 
-        return $query->bad_words;
+        return $badWords;
     }
 
     /**
@@ -51,12 +51,11 @@ trait HasBlacklist
 
     public function hasBadWords(): bool
     {
-        $query = $this->select('bad_words')->first();
-        if ($query === null) {
+        $badWords = $this->bad_words;
+        if ($badWords === null) {
             return false;
         }
 
-        $badWords = $query->bad_words;
         if (is_array($badWords) === false) {
             return false;
         }
