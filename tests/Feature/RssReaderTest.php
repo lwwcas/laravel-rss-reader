84737cb32eaba053902813656fe11523648f4abc
Lucas Duarte <lwwcas@gmail.com>
2023-04-15 16:47:50 +0100

    test: create test to create feeds class and configs

--- tests/Feature/RssReaderTest.php
+++ tests/Feature/RssReaderTest.php
@@ -142,4 +142,40 @@ class RssReaderTest extends TestCase
         $this->assertEquals($logs['key'], 'laravel-news');
         $this->assertEquals($logs['action'], RssFeedLog::ACTION_AUTOREAD);
     }
+
+    /** @test */
+    public function it_should_create_a_feed_class_that_is_registered_in_the_configuration_file()
+    {
+        config()->set('lw-rss-reader.active-rss', ['laravel-news']);
+
+        $feed = (new RssReader())->feed('laravel-news');
+        $article = $feed->first();
+
+        $this->assertNotNull($feed);
+        $this->assertNotNull($article);
+        $this->assertIsObject($feed);
+        $this->assertIsArray($article);
+        $this->assertEquals($feed->getRssFeed(), 'laravel-news');
+        $this->assertArrayHasKey('url', $article);
+        $this->assertArrayHasKey('title', $article);
+        $this->assertArrayHasKey('description', $article);
+        $this->assertArrayHasKey('date', $article);
+        $this->assertArrayHasKey('image', $article);
+        $this->assertArrayHasKey('data', $article);
+        $this->assertArrayHasKey('custom-filter', $article);
+        $this->assertArrayHasKey('black-list', $article);
+        $this->assertArrayHasKey('status', $article['black-list']);
+        $this->assertArrayHasKey('bad-words', $article['black-list']);
+    }
+
+    /** @test */
+    public function it_not_should_create_a_feed_class_that_is_not_registered_in_the_configuration_file()
+    {
+        config()->set('lw-rss-reader.active-rss', []);
+
+        $this->expectException(\LogicException::class);
+        $this->expectExceptionMessage('Your RSS feed is not active in your settings file.');
+
+        (new RssReader())->feed('laravel-news');
+    }
 }
